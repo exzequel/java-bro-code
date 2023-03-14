00f64410e490d70be7e4726551528bfe00ae600c
Exequel <exql.adzn@gmail.com>
2023-03-14 15:21:07 +0800

    add overlaoded methods - data types

--- JavaBroCode/src/OverloadedMethods.java
+++ JavaBroCode/src/OverloadedMethods.java
@@ -3,7 +3,8 @@ public class OverloadedMethods {
         // overloaded methods = methods that share the same name but have different parameters
         // method name + parameters = method signature
 
-        int x = add(1, 2, 3, 4);
+        // int x = add(1, 2, 3, 4);
+        double x = add(1.0, 2.0, 3.0, 4.0);
 
         System.out.println(x);
     
@@ -21,6 +22,21 @@ public class OverloadedMethods {
 
     static int add(int a, int b, int c, int d) {
         System.out.println("Overloaded Method 3");
+        return a + b + c + d;
+    }
+
+    static double add(double a, double b) {
+        System.out.println("Overloaded Method 4");
+        return a + b;
+    }
+
+    static double add(double a, double b, double c) {
+        System.out.println("Overloaded Method 5");
+        return a + b + c;
+    }
+
+    static double add(double a, double b, double c, double d) {
+        System.out.println("Overloaded Method 6");
         return a + b + c + d;
     }
 }
