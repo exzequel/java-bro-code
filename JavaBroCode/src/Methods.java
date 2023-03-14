f5cc8c03fd562719946e2dfd7cbb20d09d66b9cb
Exequel <exql.adzn@gmail.com>
2023-03-14 15:07:25 +0800

    return types review

--- JavaBroCode/src/Methods.java
+++ JavaBroCode/src/Methods.java
@@ -1,5 +1,30 @@
 public class Methods {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // method = block of code executed whenever called upon
+        // hello(); // invoking or calling the method
+
+        // arguments = the value you pass to a method
+        // String name = "Bro";
+        // int age = 21;
+        // hello(name, age); 
+
+        // return types
+        int x = 3;
+        int y = 4;
+
+        int z = add(x,y);
+
+    }
+    
+    // creating a new method
+    // static void hello(String name, int age) {  // parameters
+    //     System.out.println("Hello " + name);
+    //     System.out.println("You are " + age);
+    // }
+
+    static int add(int x, int y) {
+        
+        int z = x + y;
+        return z;
     }
 }
