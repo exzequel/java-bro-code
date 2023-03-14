5fb8041dbb18eda26aa4bd2f33585c54783eae61
Exequel <exql.adzn@gmail.com>
2023-03-14 15:17:52 +0800

    add overloaded methods - number of parameters are taken account

--- JavaBroCode/src/OverloadedMethods.java
+++ JavaBroCode/src/OverloadedMethods.java
@@ -1,5 +1,26 @@
 public class OverloadedMethods {
     public static void main(String[] args) {
-        
+        // overloaded methods = methods that share the same name but have different parameters
+        // method name + parameters = method signature
+
+        int x = add(1, 2, 3, 4);
+
+        System.out.println(x);
+    
+    }
+
+    static int add(int a, int b){
+        System.out.println("Overloaded Method 1");
+        return a + b;
+    }
+    
+    static int add(int a, int b, int c) {
+        System.out.println("Overloaded Method 2");
+        return a + b + c;
+    }
+
+    static int add(int a, int b, int c, int d) {
+        System.out.println("Overloaded Method 3");
+        return a + b + c + d;
     }
 }
